d4fd96b93973b5bb38378e2595c8376997b236e4
Akshat <masterakshata@gmail.com>
2020-08-26 19:37:33 +0530

    fixed typo

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,6 @@
 import { EntityManager, IDatabaseDriver, Connection } from "@mikro-orm/core";
 
 
-export type DbObjEm{
+export type DbObjEm = {
     em: EntityManager<IDatabaseDriver<Connection>>
 }
