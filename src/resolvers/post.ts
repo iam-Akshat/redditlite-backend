94b6df389a7cc24f6cc947cac7f9bb9c5f9d732a
Akshat <masterakshata@gmail.com>
2020-08-26 20:27:48 +0530

    added crud ops in post

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,10 +1,46 @@
-import { Resolver, Query, Ctx } from "type-graphql";
+import { Resolver, Query, Ctx, Arg, Int, Mutation } from "type-graphql";
 import { Post } from "../entities/Post";
 import { DbObjEm } from "../types"
+import e from "express";
 @Resolver()
 export class PostResolver{
     @Query(() => [Post])
     posts(@Ctx() {em}: DbObjEm) {   
         return em.find(Post, {});
+    }
+
+    @Query(() => Post, { nullable:true })
+    post(
+        @Arg('id',() => Int) id: number,
+        @Ctx() {em}: DbObjEm
+        ) : Promise<Post | null> {   
+        return em.findOne(Post, { id });
+    }
+
+    @Mutation(() => Post)
+    async createPost(
+        @Arg('title',() => String) title:string,
+        @Ctx() {em}: DbObjEm
+    ) : Promise<Post> {
+        const newPost = em.create(Post, { title })
+        await em.persistAndFlush(newPost)
+        return newPost
+    }
+
+    @Mutation(() => Post ,{ nullable:true })
+    async updatePost(
+        @Arg('id') id:number,
+        @Arg('title',() => String, { nullable: true }) title:string,
+        @Ctx() {em}: DbObjEm
+    ) : Promise<Post | null> {
+        const post = await em.findOne(Post, { id })
+        if(!post){
+            return null;
+        }
+        if(typeof title !== undefined){
+            post.title = title;
+            await em.persistAndFlush(post);
+        }
+        return post;
     }
 }
