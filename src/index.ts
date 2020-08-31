9323d3246f5d9000c0d0afd87a1b2df5752d039d
Akshat <masterakshata@gmail.com>
2020-08-31 20:28:39 +0530

    added query to check logged in or not

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,7 @@ const main = async () => {
     
     const app = express(); 
     app.use(cors({
-        origin:'http://localhost:3100',
+        origin:'http://localhost:3000',
         credentials:true
     }))
     app.use(
--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,8 +1,8 @@
-import { Resolver, Ctx, Arg, Mutation, InputType, Field, ObjectType } from "type-graphql";
+import { Resolver, Ctx, Arg, Mutation, InputType, Field, ObjectType, Query } from "type-graphql";
 import { User } from '../entities/User'
 import { DbObjEm } from "../types"
 import argon2 from 'argon2';
-
+import { EntityManager } from "@mikro-orm/postgresql";
 @InputType()
 class UsernamePassword{
     @Field()
@@ -31,21 +31,35 @@ class UserResponse{
 }
 @Resolver()
 export class UserResolver{
+    @Query(()=>User,{nullable:true})
+    async me(
+        @Ctx() { req,res,em }: DbObjEm
+    ){
+        if(!(req.session!.userId)) return null;
+        const user = em.findOne(User,{id:req.session!.userId})
+        return user;
+    }
+
    @Mutation(()=>UserResponse)
    async register(
        @Arg('options') options: UsernamePassword,
        @Ctx() { req,res,em }: DbObjEm
    ):Promise<UserResponse>{
     const hashedPassword = await argon2.hash(options.password)
-    const user = em.create(User, { username: options.username, password: hashedPassword })
     try {
-        await em.persistAndFlush(user);
-        req.session!.userId = user.id;
+        const result = await (em as EntityManager).createQueryBuilder(User).getKnexQuery().insert({
+            username:options.username,
+            password: hashedPassword,
+            created_at: new Date(),
+            updated_at:new Date()
+        }).returning('*');
+        const user = result[0]
+        req.session!.userId= user.id;
         return {
-            
             user
         }
     } catch (error) {
+        //console.log(error);
         if(error.code=="23505"){
             return {
                 errors:[{
@@ -55,8 +69,9 @@ export class UserResolver{
             }
         }
     }
+    
     return {
-        errors:[{field:"unknown error",message:"unknown error"}]
+        errors:[{field:"username",message:"unknown error"}]
     }
    }
 
