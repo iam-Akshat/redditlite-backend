b5996b684f972d5145c7ab63624d8c1930c0868e
Akshat <masterakshata@gmail.com>
2020-09-09 15:53:38 +0530

    changed entities from mikroorm to typeorm

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,23 +1,23 @@
-import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm"
 import { ObjectType, Field } from "type-graphql";
 
 @ObjectType()
 @Entity()
 export class Post {
     @Field()
-    @PrimaryKey()
+    @PrimaryGeneratedColumn()
     id!: number;
 
     @Field(() => String)
-    @Property({ type: 'date' } )
-    createdAt = new Date();
+    @CreateDateColumn()
+    createdAt: Date;
 
     @Field(() => String)
-    @Property({ type: 'date', onUpdate: () => new Date() })
-    updatedAt = new Date();
+    @UpdateDateColumn()
+    updatedAt: Date;
 
     @Field()
-    @Property()
+    @Column()
     title!: string;
 
 }
--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,31 +1,31 @@
-import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Entity, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, Column } from "typeorm"
 import { ObjectType, Field } from "type-graphql";
 
 @ObjectType()
 @Entity()
 export class User {
     @Field()
-    @PrimaryKey()
+    @PrimaryGeneratedColumn()
     id!: number;
 
     @Field(() => String)
-    @Property({ type: 'date' } )
-    createdAt = new Date();
+    @CreateDateColumn()
+    createdAt: Date;
 
     @Field(() => String)
-    @Property({ type: 'date', onUpdate: () => new Date() })
-    updatedAt = new Date();
+    @UpdateDateColumn()
+    updatedAt: Date;
 
     @Field(() => String)
-    @Property({ unique:true })
+    @Column({ unique:true })
     username!: string;
 
     //@Field()
-    @Property()
+    @Column()
     password!: string;
 
     @Field(()=>String)
-    @Property({unique:true})
+    @Column({unique:true})
     email!: string;
 
 }
